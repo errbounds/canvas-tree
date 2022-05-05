df6e90b83a076aebc8636d0e0921d8af52f2283a
invscks <user@user.local>
2022-05-05 17:12:20 +0900

    애니메이션 완성

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const fixedLength = 50;
 const maxDepth = 11;
+const frame = 15;
 
 class App {
   constructor() {
     this.canvas = document.getElementById("canvas");
     this.ctx = this.canvas.getContext("2d");
+    this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
 
     window.addEventListener("resize", this.resize.bind(this));
     this.canvas.addEventListener("click", this.onclick.bind(this));
-    this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
 
     this.resize();
   }
@@ -32,25 +33,25 @@ class Tree {
   constructor(posX, posY) {
     this.posX = posX;
     this.posY = posY;
-    this.branches = [];
+    this.branches = new Array(maxDepth).fill(0).map(() => []);
+    this.drawCurrentDepth = 0;
 
     this.createBranch(
       posX,
       posY,
       90 + (Math.random() * 5 - 2.5),
-      Math.random() * 30 + 70,
+      Math.random() * 50 + 70,
       0
     );
   }
 
   createBranch(startX, startY, angle, length, depth) {
-    if (depth > maxDepth) {
+    if (depth >= maxDepth) {
       return 0;
     }
     const endX = startX + length * this.cos(angle);
     const endY = startY - length * this.sin(angle);
-
-    this.branches.push(new Branch(startX, startY, endX, endY, depth));
+    this.branches[depth].push(new Branch(startX, startY, endX, endY, depth));
 
     // new branches
     const rAngle = angle + (Math.random() * 15 + 10);
@@ -65,9 +66,24 @@ class Tree {
     this.createBranch(endX, endY, lAngle, lLength, depth + 1);
   }
 
+  // finished?: true
   draw(ctx) {
-    this.branches.forEach((branch) => branch.draw(ctx));
+    if (this.drawCurrentDepth === maxDepth) {
+      cancelAnimationFrame(this.raf);
+      return true;
+    }
+
+    const isDrawing = this.branches[this.drawCurrentDepth].map((branch) =>
+      branch.draw(ctx)
+    );
+    if (isDrawing.every((branch) => branch)) {
+      this.drawCurrentDepth++;
+    }
+    this.raf = requestAnimationFrame(this.draw.bind(this, ctx));
+
+    return false;
   }
+
   cos(deg) {
     return Math.cos((Math.PI / 180) * deg);
   }
@@ -83,16 +99,36 @@ class Branch {
     this.endX = endX;
     this.endY = endY;
     this.depth = depth;
-  }
-  draw(ctx) {
-    ctx.beginPath();
 
+    this.currentFrame = 0;
+
+    this.gapX = (this.endX - this.startX) / frame;
+    this.gapY = (this.endY - this.startY) / frame;
+
+    this.currentX = this.startX;
+    this.currentY = this.startY;
+  }
+  // finished?: true
+  draw(ctx) {
+    if (this.currentFrame >= frame) {
+      return true;
+    }
+
+    console.log(`${this.currentFrame}, ${this.depth}`);
+    this.currentX += this.gapX;
+    this.currentY += this.gapY;
+
+    ctx.beginPath();
     ctx.lineWidth = maxDepth - this.depth;
+
     ctx.moveTo(this.startX, this.startY);
-    ctx.lineTo(this.endX, this.endY);
+    ctx.lineTo(this.currentX, this.currentY);
 
     ctx.closePath();
     ctx.stroke();
+
+    this.currentFrame += 1;
+    return false;
   }
 }
 
