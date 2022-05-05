596d4e7681a6f5be923403bd9ce9272bea16391d
invscks <user@user.local>
2022-05-05 21:35:10 +0900

    update

--- app.js
+++ app.js
@@ -1,134 +1,92 @@
-const fixedLength = 50;
-const maxDepth = 11;
-const frame = 15;
+import Tree, { raf } from "./tree.js";
+import data from "./data.js";
 
 class App {
   constructor() {
+    // canvas initialize
     this.canvas = document.getElementById("canvas");
     this.ctx = this.canvas.getContext("2d");
     this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
-
     window.addEventListener("resize", this.resize.bind(this));
     this.canvas.addEventListener("click", this.onclick.bind(this));
+
+    // button initialize
+    this.lightMode = true;
+    this.setBtn();
+
+    // tree
+    this.treeId = 0;
 
     this.resize();
   }
 
   resize() {
+    // canvas resize
     this.canvas.width = document.documentElement.clientWidth;
     this.canvas.height = document.documentElement.clientHeight;
     this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+    // tree animation clear
+    raf.forEach((tree) => cancelAnimationFrame(tree));
+    raf.length = 0;
+    this.treeId = 0;
+
+    // canvas clear
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 
   onclick(event) {
+    this.getData();
     const { clientX } = event;
-    const tree = new Tree(clientX, this.canvas.height);
+    const tree = new Tree(clientX, this.canvas.height, this.treeId++);
 
-    tree.draw(this.ctx);
+    tree.draw(this.ctx, this.lightMode);
   }
-}
 
-class Tree {
-  constructor(posX, posY) {
-    this.posX = posX;
-    this.posY = posY;
-    this.branches = new Array(maxDepth).fill(0).map(() => []);
-    this.drawCurrentDepth = 0;
+  setBtn() {
+    this.refreshBtn = document.getElementById("refresh");
+    this.refreshBtn.addEventListener("click", this.resize.bind(this));
 
-    this.createBranch(
-      posX,
-      posY,
-      90 + (Math.random() * 5 - 2.5),
-      Math.random() * 50 + 70,
-      0
+    this.lightModeBtn = document.getElementById("light-mode");
+    this.lightModeBtn.addEventListener(
+      "click",
+      this.lightBtnHandler.bind(this)
     );
+
+    this.darkModeBtn = document.getElementById("dark-mode");
+    this.darkModeBtn.addEventListener("click", this.darkBtnHandler.bind(this));
   }
 
-  createBranch(startX, startY, angle, length, depth) {
-    if (depth >= maxDepth) {
-      return 0;
+  lightBtnHandler() {
+    if (!this.lightMode) {
+      this.lightModeBtn.classList.remove("show");
+      this.darkModeBtn.classList.add("show");
+      this.canvas.classList.remove("black");
+      this.refreshBtn.classList.remove("white");
+      document.getElementById("panel").classList.remove("white");
+      this.lightMode = true;
     }
-    const endX = startX + length * this.cos(angle);
-    const endY = startY - length * this.sin(angle);
-    this.branches[depth].push(new Branch(startX, startY, endX, endY, depth));
-
-    // new branches
-    const rAngle = angle + (Math.random() * 15 + 10);
-    const lAngle = angle - (Math.random() * 15 + 10);
-
-    const rLength =
-      fixedLength * Math.random() * 2 * ((maxDepth - depth) / maxDepth);
-    const lLength =
-      fixedLength * Math.random() * 2 * ((maxDepth - depth) / maxDepth);
-
-    this.createBranch(endX, endY, rAngle, rLength, depth + 1);
-    this.createBranch(endX, endY, lAngle, lLength, depth + 1);
   }
+  darkBtnHandler() {
+    if (this.lightMode) {
+      this.darkModeBtn.classList.remove("show");
+      this.lightModeBtn.classList.add("show");
+      this.canvas.classList.add("black");
+      this.refreshBtn.classList.add("white");
+      document.getElementById("panel").classList.add("white");
 
-  // finished?: true
-  draw(ctx) {
-    if (this.drawCurrentDepth === maxDepth) {
-      cancelAnimationFrame(this.raf);
-      return true;
+      this.lightMode = false;
     }
-
-    const isDrawing = this.branches[this.drawCurrentDepth].map((branch) =>
-      branch.draw(ctx)
-    );
-    if (isDrawing.every((branch) => branch)) {
-      this.drawCurrentDepth++;
-    }
-    this.raf = requestAnimationFrame(this.draw.bind(this, ctx));
-
-    return false;
   }
 
-  cos(deg) {
-    return Math.cos((Math.PI / 180) * deg);
-  }
-  sin(deg) {
-    return Math.sin((Math.PI / 180) * deg);
-  }
-}
+  getData() {
+    const fixedLength = document.getElementById("fixedLength").value;
+    const lengthWeigth = document.getElementById("lengthWeight").value;
+    const maxDepth = document.getElementById("maxDepth").value;
 
-class Branch {
-  constructor(startX, startY, endX, endY, depth) {
-    this.startX = startX;
-    this.startY = startY;
-    this.endX = endX;
-    this.endY = endY;
-    this.depth = depth;
-
-    this.currentFrame = 0;
-
-    this.gapX = (this.endX - this.startX) / frame;
-    this.gapY = (this.endY - this.startY) / frame;
-
-    this.currentX = this.startX;
-    this.currentY = this.startY;
-  }
-  // finished?: true
-  draw(ctx) {
-    if (this.currentFrame >= frame) {
-      return true;
-    }
-
-    console.log(`${this.currentFrame}, ${this.depth}`);
-    this.currentX += this.gapX;
-    this.currentY += this.gapY;
-
-    ctx.beginPath();
-    ctx.lineWidth = maxDepth - this.depth;
-
-    ctx.moveTo(this.startX, this.startY);
-    ctx.lineTo(this.currentX, this.currentY);
-
-    ctx.closePath();
-    ctx.stroke();
-
-    this.currentFrame += 1;
-    return false;
+    data.fixedLength = parseInt(fixedLength);
+    data.lengthWeight = parseInt(lengthWeigth);
+    data.maxDepth = parseInt(maxDepth);
   }
 }
 
---
+++ tree.js
@@ -0,0 +1,78 @@
+import Branch from "./branch.js";
+import data from "./data.js";
+
+export const raf = [];
+
+export default class Tree {
+  constructor(posX, posY, id) {
+    this.posX = posX;
+    this.posY = posY;
+    this.id = id;
+    this.branches = new Array(parseInt(data.maxDepth)).fill(0).map(() => []);
+    // console.log(new Array(data.maxDepth));
+    this.drawCurrentDepth = 0;
+    raf.push(0);
+
+    this.createBranch(
+      posX,
+      posY,
+      90 + (Math.random() * 5 - 2.5),
+      Math.random() * 50 + 70,
+      0
+    );
+  }
+
+  createBranch(startX, startY, angle, length, depth) {
+    // create branch
+    if (depth >= data.maxDepth) {
+      return 0;
+    }
+    const endX = startX + length * this.cos(angle);
+    const endY = startY - length * this.sin(angle);
+    this.branches[depth].push(new Branch(startX, startY, endX, endY, this.id));
+
+    // new branches init
+    const rAngle = angle + (Math.random() * 15 + 10);
+    const lAngle = angle - (Math.random() * 15 + 10);
+
+    const rLength =
+      data.fixedLength *
+      Math.random() *
+      data.lengthWeight *
+      ((data.maxDepth - depth) / data.maxDepth);
+    const lLength =
+      data.fixedLength *
+      Math.random() *
+      data.lengthWeight *
+      ((data.maxDepth - depth) / data.maxDepth);
+
+    this.createBranch(endX, endY, rAngle, rLength, depth + 1);
+    this.createBranch(endX, endY, lAngle, lLength, depth + 1);
+  }
+
+  // finished?: true
+  draw(ctx, lightMode) {
+    if (this.drawCurrentDepth === data.maxDepth) {
+      cancelAnimationFrame(raf[this.id]);
+      return true;
+    }
+
+    console.log(this.drawCurrentDepth, data.maxDepth);
+    const finished = this.branches[this.drawCurrentDepth].map((branch) =>
+      branch.draw(ctx, this.drawCurrentDepth, lightMode)
+    );
+    if (finished.every((branch) => branch)) {
+      this.drawCurrentDepth++;
+    }
+    raf[this.id] = requestAnimationFrame(this.draw.bind(this, ctx, lightMode));
+
+    return false;
+  }
+
+  cos(deg) {
+    return Math.cos((Math.PI / 180) * deg);
+  }
+  sin(deg) {
+    return Math.sin((Math.PI / 180) * deg);
+  }
+}
